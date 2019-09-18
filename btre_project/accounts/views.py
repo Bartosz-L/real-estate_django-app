75213b52fa68cc6bdc6c1c1725e6f6cf707f3b5e
Bartosz Ligęza <bligeza@tidk.pl>
2019-09-18 16:10:43 +0200

    user registration

--- btre_project/accounts/views.py
+++ btre_project/accounts/views.py
@@ -1,13 +1,35 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        print('REGISTERED')
-        messages.error(request, 'Test error')
-        return redirect('register')
+        first_name = request.GET['first_name']
+        last_name = request.GET['last_name']
+        username = request.GET['username']
+        email = request.GET['email']
+        password = request.GET['password']
+        password2 = request.GET['password2']
+
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'That username is taken')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'That email is being used')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(username=username, password=password, email=email,
+                                                    first_name=first_name, last_name=last_name)
+                    user.save()
+                    messages.success(request, 'Account registered. You can log in now.')
+                    return redirect('login')
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 
