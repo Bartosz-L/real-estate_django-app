73e8f7a93d1fb90f5ab84c35a23a58f20bab7563
Bartosz Ligęza <bligeza@tidk.pl>
2019-09-20 10:03:25 +0200

    commented email config

--- btre_project/btre_project/settings.py
+++ btre_project/btre_project/settings.py
@@ -137,8 +137,8 @@ MESSAGE_TAGS = {
 }
 
 # Email config
-EMAIL_HOST = os.environ.get('EMAIL_HOST')
-EMAIL_PORT = os.environ.get('EMAIL_PORT')
-EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
-EMAIL_USE_TLS = True
+# EMAIL_HOST = os.environ.get('EMAIL_HOST')
+# EMAIL_PORT = os.environ.get('EMAIL_PORT')
+# EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+# EMAIL_USE_TLS = True
